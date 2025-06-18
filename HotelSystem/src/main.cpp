9bb57f3360d9b2bfc727a0a00f863d9c3f7ce527
GeorgiRaichev <88005729+GeorgiRaichev@users.noreply.github.com>
2025-06-18 12:10:09 +0300

    Added main

---
+++ HotelSystem/src/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "HotelSystem.h"
+#include <iostream>
+
+int main() {
+    try {
+        HotelSystem system;
+        system.run();  
+    }
+    catch (const char* msg) {
+        std::cout << "Fatal error: " << msg << "\n";
+    }
+    catch (...) {
+        std::cout << "An unexpected fatal error occurred.\n";
+    }
+
+    return 0;
+}
